94b3b7ac96a9626b5ec5901049d848e25fef8ef6
ronny <ronny_migler@mail.ru>
2022-09-15 18:29:05 +0300

    Первый коммит

---
+++ src/homework_1_9/Author.java
@@ -0,0 +1,24 @@
+package homework_1_9;
+
+public class Author {
+    private final String name;
+    private final String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Автор: " + name + " " + surname;
+    }
+}
